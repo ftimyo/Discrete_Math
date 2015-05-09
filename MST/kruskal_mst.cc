4ec5841bad8b647e11134698853103bd73f59a02
tim <tim@Narwhal.stny.rr.com>
2015-05-09 01:16:48 -0400

    update kruskal_mst using weighted quick find union method use clang++ -std=c++1y -D TEST kruskal_mst.cc -o k to generate test program

--- MST/kruskal_mst.cc
+++ MST/kruskal_mst.cc
@@ -1,6 +1,14 @@
+/*Kruskal algorithm using Weighted Quick Union Find*/
+
+#ifdef TEST
+#include <cstdio>
+#endif
+
 #include <map>
 #include <vector>
 #include <algorithm>
+#include <numeric>
+#include <iterator>
 using uint = unsigned;
 struct Edge {
 	uint v, w; float d;
@@ -8,26 +16,60 @@ struct Edge {
 
 class Kruskal {
  public:
-	std::map<uint, uint> group;
+	std::vector<int> sz;
+	std::vector<uint> v_set;
 	std::vector<Edge> mst;
 	uint find(uint v);
-	Kruskal(const std::vector<Edge>& E);
+	void union_pair(uint v, uint w);
+	Kruskal(std::vector<Edge>& E, std::size_t N);
 };
 
-uint Kruskal::find(uint v) {
-	if (__builtin_expect(group.find(v) == end(group), false)) group[v] = v;
-	return v == group[v] ? v : group[v] = find(v);
+void Kruskal::union_pair(uint v, uint w) {
+	auto i = find(v);
+	auto j = find(w);
+	if (i == j) return;
+	if (sz[i] < sz[j]) {v_set[i] = j; sz[j] += sz[i];}
+	else {v_set[j] = i; sz[i] += sz[j];}
 }
 
-Kruskal::Kruskal(const std::vector<Edge>& E) {
+uint Kruskal::find(uint v) {
+	return v == v_set[v] ? v : v_set[v] = find(v_set[v]);
+}
+
+Kruskal::Kruskal(std::vector<Edge>& E, std::size_t N)
+		: sz(N, 1),
+			v_set(N, 0) {
+
+
+	std::iota(std::begin(v_set), std::end(v_set), 0);
+
 	auto weight_cmp = [] (const auto& e1, const auto& e2) {
 		return e1.d < e2.d;};
 	std::sort(begin(E), end(E), weight_cmp);
+
 	for (auto e : E) {
 		if (find(e.v) == find(e.w)) continue;
-		group[find(e.v)] = find(e.w);
+		union_pair(e.v, e.w);
 		mst.push_back(e);
 	}
 }
 
-int main() {return 0;}
+int main() {
+#ifdef TEST
+	std::vector<Edge> e_set{{0,3,3},{3,6,5},{3,6,6},{0,5,1},{5,0,2},{6,7,3},
+		{5,7,1},{7,8,4},{1,0,5},{5,8,2},{5,4,0},{1,4,-3},{4,8,5},{8,8,2},{4,2,-1}};
+
+	printf("input Edge set\n");
+	for (const auto& e : e_set) {
+		printf("%u->%u : %f\n", e.v, e.w, e.d);
+	}
+
+	Kruskal k(e_set, 9);
+
+	printf("output Edge set\n");
+	for (const auto& e : k.mst) {
+		printf("%u->%u : %f\n", e.v, e.w, e.d);
+	}
+#endif
+	return 0;
+}
